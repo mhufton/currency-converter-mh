70e8be632b4484a54aac3e8c0e6f26356bdbe3dc
Mike Hufton <mikehufton@Mikes-MacBook.attlocal.net>
2022-06-29 08:08:28 -0700

    added stylings for the dropdown menus so the flag is next to country code

--- src/Components/CurrencySelector/CurrencySelector.js
+++ src/Components/CurrencySelector/CurrencySelector.js
@@ -23,8 +23,11 @@ export default function CurrencySelector({ setConvertForm, toOrFrom }) {
       <div className="w-[350px] mb-4 text-xl z-[9999]">
         <Listbox value={selectedCode} onChange={setSelectedCode}>
           <div className="relative mt-1 z-10">
-            <Listbox.Button className="relative z-10 w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-              <span className="block truncate z-10">{selectedCode}</span>
+            <Listbox.Button className="text-center relative z-10 w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+              <div className="flex flex-row justify-center pl-2">
+                <span className="block truncate z-10">{data[selectedCode].flag}&nbsp;</span>
+                <span className="block truncate z-10">&nbsp;{selectedCode}</span>
+              </div>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <SelectorIcon
                   className="h-5 w-5 text-gray-400 z-10"
@@ -38,7 +41,7 @@ export default function CurrencySelector({ setConvertForm, toOrFrom }) {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute z-[9999] mt-1 max-h-60 w-full overflow-y-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <Listbox.Options className="absolute z-[9999] mt-1 max-h-60 w-full overflow-y-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                 {customCodes.map((c, index) => (
                   <Listbox.Option
                     key={index}
@@ -78,8 +81,11 @@ export default function CurrencySelector({ setConvertForm, toOrFrom }) {
       <div className="w-[350px] mb-4 text-xl">
         <Listbox value={selectedCode} onChange={setSelectedCode}>
           <div className="relative mt-1 z-50">
-            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-              <span className="block truncate">{selectedCode}</span>
+            <Listbox.Button className="text-center relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+              <div className="flex flex-row justify-center pl-2">
+                <span className="block truncate z-10">{data[selectedCode].flag}&nbsp;</span>
+                <span className="block truncate z-10">&nbsp;{selectedCode}</span>
+              </div>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <SelectorIcon
                   className="h-5 w-5 text-gray-400"
